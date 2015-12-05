36011e7dd4e98e6e89b5a1f93b2a60d195a6369e
BurgerLUA <mattwasnothere@gmail.com>
2015-12-04 18:00:15 -0800

    QUACK

---
+++ lua/autorun/sh_sharedstuff.lua
@@ -0,0 +1,22 @@
+
+
+
+for i=1,5 do
+	util.PrecacheSound("ambient_mp3/bumper_car_quack"..i..".mp3")
+end
+
+
+function BetterFootsteps(ply,pos,foot,sound,volume,filter)
+
+
+	local Velocity = ply:GetVelocity():Length()
+	
+	if Velocity > 450 and ply:KeyDown(IN_SPEED) then
+		sound = "ambient_mp3/bumper_car_quack"..math.random(1,5)..".mp3"
+		EmitSound(sound,pos,ply:EntIndex(),CHAN_BODY,volume,SNDLVL_180dB,SND_NOFLAGS,100)
+		return true
+	end
+
+end
+
+hook.Add("PlayerFootstep","Better Footsteps",BetterFootsteps)
