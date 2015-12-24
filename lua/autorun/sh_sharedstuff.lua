d5dee48d435839f49eb889f51cb0eaa2c6be0988
BurgerLUA <mattwasnothere@gmail.com>
2015-12-23 21:32:54 -0800

    Fuck Ducks

--- lua/autorun/sh_sharedstuff.lua
+++ lua/autorun/sh_sharedstuff.lua
@@ -8,7 +8,7 @@ end
 
 function BetterFootsteps(ply,pos,foot,sound,volume,filter)
 
-
+--[[
 	local Velocity = ply:GetVelocity():Length()
 	
 	if Velocity > 450 and ply:KeyDown(IN_SPEED) then
@@ -16,6 +16,7 @@ function BetterFootsteps(ply,pos,foot,sound,volume,filter)
 		EmitSound(sound,pos,ply:EntIndex(),CHAN_BODY,volume,SNDLVL_180dB,SND_NOFLAGS,100)
 		return true
 	end
+--]]
 
 end
 
