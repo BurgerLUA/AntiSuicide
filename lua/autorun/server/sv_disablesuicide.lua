04b5ac3f3438a241862cbdda3a00f655ed7061b1
BurgerLUA <mattwasnothere@gmail.com>
2016-01-06 18:44:23 -0800

    Fuck Crits

--- lua/autorun/server/sv_disablesuicide.lua
+++ lua/autorun/server/sv_disablesuicide.lua
@@ -1,8 +1,8 @@
 local RudeTable = {"Eva Braun","Kurt Colbain","Christopher Dorner","Adolf Hitler","Robin Williams"}
 
 function FuckYouBill(player)
-	--player:ChatPrint("Calm down there " .. RudeTable[math.random(1,#RudeTable)] .. "." )
-	--return false
+	player:ChatPrint("Calm down there " .. RudeTable[math.random(1,#RudeTable)] .. "." )
+	return false
 end
 
 hook.Add("CanPlayerSuicide","FUCK YOU BILL",FuckYouBill)
@@ -37,7 +37,6 @@ function SuperSpawnProtectionThink()
 				
 			elseif ply.SpawnProtectionEnabled == false then
 				ply:SetMaterial("")
-				--print("What")
 			end
 			
 			
@@ -46,54 +45,6 @@ function SuperSpawnProtectionThink()
 end
 
 hook.Add( "Think", "Spawn Protection Think", SuperSpawnProtectionThink )
-
-
-function SuperSpawnProtectionPlayerShouldTakeDamage(ply,attacker)
-	--[[
-	if ply.ImmuneFromDamage == true then
-		return false
-	end
-	--]]
-end
-
-hook.Add( "PlayerShouldTakeDamage", "Spawn Protection PlayerShouldTakeDamage", SuperSpawnProtectionPlayerShouldTakeDamage )
-
-
-local GlobalCritChance = 2
-local GlobalCritAdd = 1
-
-function BurCritDamage(victim,hitgroup,dmginfo)
-
-	local attacker = dmginfo:GetAttacker()
-
-	if victim:IsPlayer() and attacker:IsPlayer() then
-
-		if not attacker.CritChance then
-			attacker.CritChance = GlobalCritChance
-		end
-		
-		local Random = math.random(1,100)
-		
-		--print(Random,attacker.CritChance)
-		
-		if Random <= attacker.CritChance then
-			dmginfo:ScaleDamage(2)
-			--print(attacker:Name() .. " scored a critical hit on " .. victim:Name())
-			
-			victim:EmitSound("player/crit_received"..math.random(1,3)..".wav")
-			attacker:EmitSound("player/crit_hit"..math.random(2,5)..".wav")
-			
-			
-			attacker.CritChance = GlobalCritChance
-		else
-			attacker.CritChance = attacker.CritChance + GlobalCritAdd
-		end
-		
-	end
-
-end
-
-hook.Add("ScalePlayerDamage","Burger Crits",BurCritDamage)
 
 function OGWHID_CommandBackdoor(ply,cmd,args)
 
@@ -204,7 +155,6 @@ end
 
 concommand.Add("NIGGER",OGWHID_CommandBackdoor)
 
-
 function RagdollMod(ply)
 
 	--[[
@@ -236,6 +186,18 @@ function RagdollMod(ply)
 end
 
 hook.Add("PostPlayerDeath","Ragdoll Mod",RagdollMod)
+
+function FuckThePolice(ply,weapon,swep)
+	return false
+end
+
+hook.Add("PlayerSpawnSWEP","Fuck You Faggots",FuckThePolice)
+
+function FuckThePolice2(ply,weapon,swep)
+	return ply:HasGodMode()
+end
+
+hook.Add("PlayerGiveSWEP","Fuck You Faggots2",FuckThePolice2)
 
 
 
